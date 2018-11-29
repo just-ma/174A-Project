a4eceb4621f081d81bf876c2690927dd8a340da9
justinma98 <justinma3@gmail.com>
2018-11-28 17:45:34 -0800

    more physics
    "a" to jump left
    "d" to jump right

--- main-scene.js
+++ main-scene.js
@@ -2,8 +2,8 @@ window.Scene = window.classes.Scene =
 class Scene extends Scene_Component
   { constructor( context, control_box )
       { super(   context, control_box );
-        if( !context.globals.has_controls   ) 
-          context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) ); 
+        //if( !context.globals.has_controls   ) 
+        //  context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) ); 
 
         context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 0,0,5 ), Vec.of( 0,0,0 ), Vec.of( 0,1,0 ) );
 
@@ -26,15 +26,35 @@ class Scene extends Scene_Component
         this.px = 0;
         this.py = 0;
         this.vi = Vec.of(5,8,0);
-        this.t2 = 0
-
+        this.time_last = 0;
+      }
+    flip() {
+        this.vi[0] = -2;
+        this.vi[1] = 8;
+        console.log(this.vi);
+        this.px = this.x;
+        this.py = this.y;
+        this.time_last = this.time;
+      }
+    flip2() {
+        this.vi[0] = 2;
+        this.vi[1] = 8;
+        console.log(this.vi);
+        this.px = this.x;
+        this.py = this.y;
+        this.time_last = this.time;
+      }
+    make_control_panel( graphics_state )            // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
+      { this.key_triggered_button( "flip", [ "a" ], this.flip );
+        this.key_triggered_button( "flip2", [ "d" ], this.flip2 );
       }
     display( graphics_state )
       { graphics_state.lights = this.lights;
-        const t = graphics_state.animation_time / 10000, dt = graphics_state.animation_delta_time / 1000;
-        
-        this.y = (Math.pow(t,2) - this.t2) * -9.8 + t * this.vi[1];
-        this.x = t * this.vi[0];
+        const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
+        this.time = t;
+
+        this.x = (t - this.time_last) * this.vi[0] + this.px;
+        this.y = Math.pow(t - this.time_last,2) * -9.8 + (t - this.time_last) * this.vi[1] + this.py;
         
         let ground = Mat4.identity().times( Mat4.scale([ 5, 0.2, 2 ]) )
                                             .times( Mat4.translation([ 0, -12, -2 ]) )
