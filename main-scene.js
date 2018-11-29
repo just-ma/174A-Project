b25d174e294dfd63c55c079b49731ec99c0019b5
justinma98 <justinma3@gmail.com>
2018-11-28 18:41:02 -0800

    primitive collisions
    only checks floor

--- main-scene.js
+++ main-scene.js
@@ -27,35 +27,42 @@ class Scene extends Scene_Component
         this.py = 0;
         this.x_last = 0;
         this.y_last = 0;
-        this.vi = Vec.of(5,8,0);
+        this.vi = Vec.of(0,8,0);
         this.time_last = 0;
         this.velocity = Vec.of(0,0,0);
+        this.grounded = false;
       }
-    flip() {
-        this.vi[0] = -5 + this.velocity[0];
-        this.vi[1] = 10 + this.velocity[1];
-        console.log(this.vi);
+    add_force( force ){
+        this.vi[0] = force[0] + this.velocity[0];
+        this.vi[1] = force[1] + this.velocity[1];
         this.x_last = this.x;
         this.y_last = this.y;
         this.time_last = this.time;
+    }
+    flip() {
+        this.add_force( Vec.of(-5,10,0) );
       }
     flip2() {
-        this.vi[0] = 5 + this.velocity[0];
-        this.vi[1] = 10 + this.velocity[1];
-        console.log(this.vi);
-        this.x_last = this.x;
-        this.y_last = this.y;
-        this.time_last = this.time;
+        this.add_force( Vec.of(5,10,0) );
       }
     make_control_panel()
       { this.key_triggered_button( "flip", [ "a" ], this.flip );
         this.key_triggered_button( "flip2", [ "d" ], this.flip2 );
       }
+    check_collisions()
+      { if (this.y < -2){
+            this.velocity[0] *= 0.8;
+            this.velocity[1] *= -0.8;
+            this.y = -2;
+            this.add_force( Vec.of(0,0,0) );
+        }
+      }  
     display( graphics_state )
       { graphics_state.lights = this.lights;
         const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
         this.time = t;
 
+        //set new positions
         this.px = this.x;
         this.py = this.y
 
@@ -63,10 +70,16 @@ class Scene extends Scene_Component
         this.y = Math.pow(t - this.time_last,2) * -9.8 + (t - this.time_last) * this.vi[1] + this.y_last;
         
         this.velocity = Vec.of(50*(this.x - this.px), 50*(this.y - this.py), 0);
-        console.log(this.velocity);
+        
 
-        let ground = Mat4.identity().times( Mat4.scale([ 5, 0.2, 2 ]) )
-                                            .times( Mat4.translation([ 0, -12, -2 ]) )
+        //check for collisions 
+        this.check_collisions();
+
+        
+        //draw objects
+        let ground = Mat4.identity().times( Mat4.translation([ 0, -3, -4 ]) )
+                                    .times( Mat4.scale([ 5, 0.1, 2 ]) )
+                                    
         this.shapes.box.draw( graphics_state, ground, this.materials.phong );
 
         let ball_transform = Mat4.identity().times(Mat4.translation([ this.x, this.y, -4 ]) )
